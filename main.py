1bcddd2d25826fd74083771e7d00299fffccfe8e
ctsvs6688-gif <ctsvs6688@gmail.com>
2026-01-11 15:16:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 import logging
 
 # ================= 配置 =================
-BOT_TOKEN = "8228263725:AAG9AK5A-0kZOJHBzuUYqLHjjkG2jW54Vt4"
+BOT_TOKEN = "8228263725:AAHT6uoVxhqSuPlShphz7gFqwvtEpmdmkj4"
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "-7571918976"))
 
 # ================= 日志 =================
